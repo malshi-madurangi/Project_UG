1555e1154d89aa156c49ea211e48937ef592b392
malshi-madurangi <m.madurangidesilva@gmail.com>
2022-01-19 12:43:38 +0530

    Host to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const path = require('path');
 
 const app = express();
 
@@ -21,6 +22,16 @@ app.use('/api/players', require('./routes/api/players'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/authPlayer', require('./routes/api/authPlayer'));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
